e018978483e3c467c3fbe43859652176ab76d292
Wowo10 <wojtekwojtek529@gmail.com>
2021-07-04 17:46:26 +0200

    Added calling the sh command.

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
 
 func main() {
-	fmt.Println("dupa")
+	argsWithoutProg := os.Args[1:]
+
+	execPath, err := exec.LookPath(argsWithoutProg[0])
+
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+
+	cmd := &exec.Cmd{
+		Path:   execPath,
+		Args:   argsWithoutProg,
+		Stdout: os.Stdout,
+		Stderr: os.Stdout,
+	}
+
+	if err := cmd.Run(); err != nil {
+		fmt.Println("Error:", err)
+	}
 }
---
+++ scripts/callMe.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+if [ -z "$1" ]
+then
+    echo 'Hi! Noname!'
+else
+    echo 'Hi! '$1!
+fi
