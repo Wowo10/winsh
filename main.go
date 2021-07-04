7f0df3a919247d79545a7aec8b425891705736a1
Wowo10 <wojtekwojtek529@gmail.com>
2021-07-04 19:20:14 +0200

    Working example.

--- main.go
+++ main.go
@@ -1,28 +1,43 @@
 package main
 
 import (
-	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 func main() {
 	argsWithoutProg := os.Args[1:]
 
-	execPath, err := exec.LookPath(argsWithoutProg[0])
+	content, err := ioutil.ReadFile(argsWithoutProg[0])
 
 	if err != nil {
-		fmt.Println("Error: ", err)
+		panic(err)
+	}
+
+	contentStr := string(content)
+	contentStr = strings.ReplaceAll(contentStr, "\r\n", "\n")
+
+	const tempFilePath = "./temp/temp.sh"
+
+	ioutil.WriteFile(tempFilePath, []byte(contentStr), 0)
+
+	execPath, err := exec.LookPath(tempFilePath)
+
+	if err != nil {
+		panic(err)
 	}
 
 	cmd := &exec.Cmd{
 		Path:   execPath,
-		Args:   argsWithoutProg,
+		Args:   append([]string{execPath}, argsWithoutProg[1:]...),
 		Stdout: os.Stdout,
 		Stderr: os.Stdout,
 	}
 
 	if err := cmd.Run(); err != nil {
-		fmt.Println("Error:", err)
+		panic(err)
 	}
+
 }
